b5d27f01f3430fac2bfbeca24bc292f3d491404a
Wyatt H <jhawes@ucsc.edu>
2023-05-04 15:20:00 -0700

    Added spinning pictures

--- game.js
+++ game.js
@@ -107,7 +107,7 @@ class Intro extends Phaser.Scene {
 }
 class keyBreak extends AdventureScene{
     constructor(){
-        super('keybreak')
+        super('keybreak');
     }
     
     preload(){
@@ -116,6 +116,7 @@ class keyBreak extends AdventureScene{
         this.load.image('key_body', 'key_body.png');
         this.load.image('key_blade', 'key_blade.png');
     }
+    
     create(){
         this.cameras.main.fadeIn(3000, 255,255,255);
         this.cameras.main.setBackgroundColor('#EAEAEA');
@@ -156,18 +157,23 @@ class keyBreak extends AdventureScene{
                 shk.paused = false;
             }
         });
+        let dura = 50;
         let shk = this.tweens.add({
             targets: this.key,
             repeat: 15,
             angle: {from: -5, to: 5},
+            duration: dura,
             yoyo: true,
-            duration: 50,
             paused: true,
+            onRepeat: function(){
+                dura -=15;
+                //console.log(dura);
+            },
             onComplete: function(){
                 brk1.paused = false;
                 brk2.paused = false;
                 brk3.paused = false;
-            }
+            },
         })
         let brk1 = this.tweens.add({
             targets: head,
@@ -181,11 +187,16 @@ class keyBreak extends AdventureScene{
             y:  -1000 * (Math.random() + 0.3),
             paused: true 
         });
+        let myvar = this;
+        console.log(myvar)
         let brk3 = this.tweens.add({
             targets: blade,
             x:  1000 * (Math.random() + 0.3) ,
             y:  1000 * (Math.random() + 0.3),
-            paused: true 
+            paused: true,
+            onComplete: function(){
+               myvar.gotoScene('menu');
+            }
         });
     }
     
@@ -193,6 +204,95 @@ class keyBreak extends AdventureScene{
     update(){
     }
 }
+
+class Menu extends AdventureScene{
+    constructor() {
+        super('menu');
+    }
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('puz1', '1.jpg');
+        this.load.image('puz2', '2.jpg');
+        this.load.image('puz3', '4.jpg');
+    }
+
+    create(){
+        this.cameras.main.setBackgroundColor('#EAEAEA');
+
+        // this.rotate = this.add.container(this.cameras.main.worldView.x, this.cameras.main.worldView.y);
+        // let p1 = this.add.sprite(0,0, 'puz1');
+        // p1.setScale(.5);
+
+        // let p2 = this.add.sprite(800,200, 'puz2');
+        // p2.setScale(.25);
+
+        // let p3 = this.add.sprite(800,700, 'puz3');
+        // p3.setScale(.25);
+
+        //From https://phaser.discourse.group/t/how-to-create-circular-movement-motion-for-a-gameobject-with-arcade-physics/8324/4
+        var graphics = this.add.graphics().lineStyle(1, 0xFF0000, 0.5);
+        // A circle curve starts at angle 0, the "3 o'clock" position.
+        // Make a circle path of radius 200 starting from (600, 200)
+        var path1 = new Phaser.Curves.Path(900, 500).ellipseTo(400,400, 0, 360, false, 0);
+        var path2 = new Phaser.Curves.Path(300, 845).ellipseTo(400,400, 0, 360, false, 120);
+        var path3 = new Phaser.Curves.Path(300, 153).ellipseTo(400,400, 0, 360, false, 240);
+        //path1.draw(graphics, 128);
+        //path2.draw(graphics, 128);
+        //path3.draw(graphics, 128);
+
+        var b1 = this.add.follower(path1, 900, 500, 'puz1');
+        b1.setScale(.25);
+        var b2 = this.add.follower(path2, 300, 845, 'puz2');
+        b2.setScale(.25);
+        var b3 = this.add.follower(path3, 300, 153, 'puz3');
+        b3.setScale(.25);
+        {
+            //block.setScale(.25);
+            // Physics
+            this.physics.add.existing(b1);
+            //this.physics.add.existing(b2);
+           //this.physics.add.existing(b3);
+            // Prevent drift?
+            //b1.body.moves = false;
+            b1.startFollow({
+                duration: 10000,
+                yoyo: false,
+                loop: -1,
+                onStart: function () {
+                path1.getPoint(0, b1.pathVector);
+                }
+            });
+            //b2.body.moves = false;
+            b2.startFollow({
+            //delay: 3334,
+            duration: 10000,
+            yoyo: false,
+            loop: -1,
+            onStart: function () {
+            path2.getPoint(0, b2.pathVector);
+            }});
+
+            //b3.body.moves = false;
+            b3.startFollow({
+            //delay: 6667,
+            duration: 10000,
+            yoyo: false,
+            loop: -1,
+            onStart: function () {
+            path3.getPoint(0, b3.pathVector);
+            }
+        });
+        }
+
+    }
+
+
+
+}
+
+
+
+
 
 class Outro extends Phaser.Scene {
     constructor() {
@@ -207,13 +307,18 @@ class Outro extends Phaser.Scene {
 
 
 const game = new Phaser.Game({
+    //type: Phaser.CANVAS,
+    physics: {
+        default: 'arcade',
+        arcade: { debug: true }
+    },
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1920,
         height: 1080
     },
-    scene: [keyBreak, Intro, Demo2, Outro],
+    scene: [Menu, keyBreak, Intro, Demo2, Outro],
     title: "Adventure Game",
 });
 
